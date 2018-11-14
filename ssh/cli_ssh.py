7ba1730215ba0ad3ec4476267c33dab9483fca85
SeanWanghp <maojunwanghp@gmail.com>
2018-11-14 16:53:35 +0800

    ssh

--- ssh/cli_ssh.py
+++ ssh/cli_ssh.py
@@ -1,7 +1,15 @@
-import paramiko, logging
-import sys
-sys.path.append('...')
+# coding=utf-8
+#C:\Python27\Doc python
+__author__='Sean Wang'
+#data@:2018-11-14
+#print out.decode('gbk').encode('utf-8')   #output have Chinese word and English word
 from basic_lib.decorator.log import Log
+import paramiko, sys
+sys.path.append('...')
+'''
+paramiko是用python语言写的一个模块，遵循SSH2协议，支持以加密和认证的方式，进行远程服务器的连接。
+'''
+
 
 def log(text):
     def decorator(func):
@@ -10,6 +18,7 @@ def log(text):
             return func(*args, **kw)
         return wrapper
     return decorator
+
 
 def sshclient_execmd(hostname, port, username, password, execmd):
     paramiko.util.log_to_file("paramiko.log")
@@ -20,10 +29,21 @@ def sshclient_execmd(hostname, port, username, password, execmd):
     for command in execmd.splitlines():
         stdin, stdout, stderr = ssh.exec_command(command)
         # stdin.write("Y")  # Generally speaking, the first connection, need a simple interaction.
-
         print stdout.read()
 
     ssh.close()
+
+
+def with_open(filename):
+    with open(filename, 'rt') as f:
+        comm = ''
+        for lineno, line in enumerate(f, 1):
+            for command in line.split(','):
+                command.strip()
+                comm += command
+        print 'command: ', comm
+    return comm
+
 
 @log("start to running")
 def main():
@@ -31,13 +51,8 @@ def main():
     port = 22
     username = 'sysadmin'
     password = 'sysadmin'
-    execmd = '''paginate false
-                show card
-                show version
-                show igmp
-                show vlan
-                show interface summary'''
-
+    filename = 'cmd.txt'
+    execmd = with_open(filename)
     sshclient_execmd(hostname, port, username, password, execmd)
 
 
